480f39fd8efa8c138399e8a0f9eb1393aa5deabe
hashy <hashy@tutanota.de>
2023-11-13 21:32:28 -0500

    NULL

---
+++ server.py
@@ -0,0 +1,24 @@
+
+from flask import Flask, render_template, request
+from geopy.geocoders import Nominatim
+
+
+server = Flask(__name__)
+geolocator = Nominatim(user_agent="Python3 on Linux")
+
+
+@server.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        address = geolocator.geocode(request.form["address"])
+
+        return render_template("index.html", address = address.raw)
+    
+    else:
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    server.run("127.0.0.1", 1984)
+
+
